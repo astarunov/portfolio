0e61f485033d6a23046800f6dc5097dcd503cd7b
astarunov <astarunov444@gmail.com>
2025-01-06 02:00:14 +0100

    stack fix

--- src/components/Interest.tsx
+++ src/components/Interest.tsx
@@ -6,7 +6,7 @@ interface InterestProps {
 
 const Interest: React.FC<InterestProps> = ({ interst }) => {
   return (
-    <div className="cursor-pointer border border-white min-w-[100px] text-[40px] rounded-[50px] h-[70px] flex items-center justify-center px-4 hover:bg-white hover:text-[#1F1F1F] transition duration-500">
+    <div className="cursor-pointer border border-white min-w-[100px] text-[25px] md:text-[40px] rounded-[50px] h-[40px] md:h-[70px] flex items-center justify-center px-4 hover:bg-white hover:text-[#1F1F1F] transition duration-500">
       {interst}
     </div>
   );
